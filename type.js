2c6c753659e8d8f774864217080faf465d29af11
shreyag12 <gupta.shreya1203@gmail.com>
2018-06-20 20:39:36 +0530

    my portfolio

---
+++ type.js
@@ -0,0 +1,13 @@
+ // $(function(){
+ // 	var typed = new Typed('.typed', {
+ //    stringsElement: '#typed-strings',
+ //    typeSpeed: 30,
+ //  });
+ // });
+
+ $(function(){
+	$(".typed").typed({
+		strings: ["Hi, I am a newbie in web development and aspire to become a full stack developer\n\nSkills: HTML,CSS, Javascript, Python\nFrameworks: Bootstrap, Django"],
+		typeSpeed: 1,
+	});
+});
